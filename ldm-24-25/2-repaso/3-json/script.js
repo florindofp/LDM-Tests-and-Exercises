844d03729de2c37a25e21a4d338f238f25111adc
Florindo <flori_89_ld@hotmail.com>
2025-04-02 10:49:49 +0200

    fix html json

--- ldm-24-25/2-repaso/3-json/script.js
+++ ldm-24-25/2-repaso/3-json/script.js
@@ -1,11 +1,24 @@
-fetch("datos.json")
-    .then(response => response.json())
-    .then(data => {
-        let tabla = document.getElementById("tablaBody");
-        data.forEach(usuario => {
-            let fila = document.createElement("tr");
-            fila.innerHTML = `<td>${usuario.nombre}</td><td>${usuario.edad}</td><td>${usuario.email}</td>`;
-            tabla.appendChild(fila);
-        });
-    })
-    .catch(error => console.error("Error cargando JSON:", error));
+
+let tabla = document.getElementById("tablaBody");
+data = [
+    {
+      "nombre": "Juan",
+      "edad": 25,
+      "email": "juan@mail.com"
+    },
+    {
+      "nombre": "Ana",
+      "edad": 30,
+      "email": "ana@mail.com"
+    },
+    {
+      "nombre": "Carlos",
+      "edad": 22,
+      "email": "carlos@mail.com"
+    }
+];
+data.forEach(usuario => {
+    let fila = document.createElement("tr");
+    fila.innerHTML = `<td>${usuario.nombre}</td><td>${usuario.edad}</td><td>${usuario.email}</td>`;
+    tabla.appendChild(fila);
+});
